3c804d18cf38ad26adad00fde8478f6d9f60e3c2
cherriesareodd <horizonsbyxryn@gmail.com>
2023-08-30 20:28:07 -0400

    Initial Commit

---
+++ NA-1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NA-1
+//
+//  Created by Matthew Smith on 8/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NA-1/NA_1App.swift
@@ -0,0 +1,17 @@
+//
+//  NA_1App.swift
+//  NA-1
+//
+//  Created by Matthew Smith on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct NA_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
